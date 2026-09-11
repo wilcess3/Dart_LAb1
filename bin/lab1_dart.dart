e5e938d7fa479216084cc244a9f3f8497997c849
wilcess3 <nastyakhodyreva1994@gmail.com>
2026-09-11 13:11:22 +0300

    learn dart cycles

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -94,5 +94,47 @@ void main() {
    ((name)=> name.length > 4).toList();
    print(longNames);
 
+  int score2 = 85;
+  String grade;
+  if (score >= 90) {
+    grade = 'A';
+  } else if (score2 >= 75) {
+    grade = 'B';
+  } else {
+    grade = 'C';
+  }
+  print(grade);
+  String result = score >= 60 ? 'Сдал' : 'Не сдал';
+  print(result);
+
+  for (int i = 0; i < 5; i++){
+    print(i);
+  }
+
+  List<String> fruits3 = ['яблоко','банан','груша'];
+  for (var fruit in fruits3) {
+    print(fruit);
+  }
+
+  int n = 0;
+  while (n>3) {
+    print(n);
+    n++;
+  }
+
+  String day = 'Пн';
+  switch (day) {
+    case 'Сб':
+    case 'Вс':
+      print('Выходной');
+      break;
+    case 'Пн':
+      print('Начало недели');
+      break;
+    default:
+      print('Рабочий день');
+
+  }
+
 
 }
